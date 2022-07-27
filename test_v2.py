58531738b972834137c8353c9f943b11c0fc05fb
WikiGenius <muhammedelyamani92@gmail.com>
2022-07-27 06:28:31 +0200

    add input interface & test 2 cases

--- test_v2.py
+++ test_v2.py
@@ -5,27 +5,55 @@ import numpy as np
 from mission_controller_v2.mission_controller import MissionController
 from mission_controller_v2.simulated_robot import SimulatedRobot
 
+'''
+input data
+I: initial point
+T: trajectories
+N: data points in each trajectory
+N data points each in line x,y: line sperated space 
+''' 
+
+class System:
+    def __init__(self) -> None:
+        self.initial_position = None
+        self.trajectory = None
+
+    def __get_initial_position(self):
+        l1=list(map(float, input().strip().split(' ')))
+        self.initial_position = np.array(l1)
+
+    def __get_input_data(self):
+        
+        T= int(input().strip())
+        for i in range(T):
+            l2 = []
+            N= int(input().strip())
+            for j in range(N):
+                l2.append(list(map(float, input().strip().split(' '))))
+            trajectory = np.array(l2)
+            yield trajectory
+
+    def run_robot(self):
+        self.__get_initial_position()
+        if self.initial_position is None:
+            raise Exception("Need Initial position")
+        simulated_robot = SimulatedRobot(self.initial_position)
+        controller = MissionController(simulated_robot)
+
+        for self.trajectory in self.__get_input_data():
+
+            # set the trajectory comming from user input
+
+            controller.set_trajectory(self.trajectory)
+
+            time.sleep(2.5)
+
+        thread_poll_position = controller.thread_poll_position
+        thread_poll_position.join()
 
 def test_normal_operation():
-
-    simulated_robot = SimulatedRobot(np.array([0.0, 0.0]))
-    controller = MissionController(simulated_robot)
-
-    # set the first trajectory
-
-    controller.set_trajectory(np.array([[0.0, 0.0], [1.0, 0.0], [2.0, 0.0]]))
-
-    time.sleep(2.5)
-
-    # # now set a different trajectory before the first trajectory completes
-    # print("X")
-    controller.set_trajectory(np.array([[2.0, 0.0], [3.0, 0.0], [3.0, 1.0]]))
-
-    time.sleep(2.5)
-    thread_poll_position = controller.thread_poll_position
-    thread_poll_position.join()
+    System().run_robot()
     print("Test complete")
-
     exit(0)
 
 
